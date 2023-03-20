fcaf319707af99cdefe3b0223ad9bcb062ed38aa
jburns962 <126725972+BurniesG@users.noreply.github.com>
2023-03-20 05:22:58 +0000

    complete

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    Celclus = int(input('enter the celclus degree: '))
+    fahrenheit = (9.0 / 5.0) * celcius + 32
+    print(f'the fahrenheit is {fahreheit:.2f}')
+
     pass
 
 
